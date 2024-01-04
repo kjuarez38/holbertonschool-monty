b67f623c3b066016d052bfa1a71b02714a0cf4e2
kjuarez38 <kaisjuarez@gmail.com>
2024-01-04 05:03:40 -0800

    .c

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * stack sets the format of the data to a stack (LIFO)
+ * @stack: Pointer to the stack.
+ * @line_number: to follow the number line of the reading of the files
+ */
+void stack(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+	arguments->stack = 1;
+}
