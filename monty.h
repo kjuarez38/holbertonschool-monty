23ce16a7fcc28db5f8cce2472539342aa17079f3
kjuarez38 <kaisjuarez@gmail.com>
2024-01-05 02:34:12 -0800

    .c

--- monty.h
+++ monty.h
@@ -1,24 +1,27 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -31,11 +34,13 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void push(stack_t stack, int value);
-void pall(stack_t *stack, unsigned int line_number);
+int push(stack_t **stack, char *value);
+int pall(stack_t **stack);
+int line_scanner(FILE **file, char **line);
+int find_function(stack_t **stack, char *command, char *value);
 
 #endif
