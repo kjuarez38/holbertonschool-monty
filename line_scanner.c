20f76a26d5bbefad9f4dd0170fed656a4722c93f
kjuarez38 <kaisjuarez@gmail.com>
2024-01-06 09:41:39 -0800

    .c

--- line_scanner.c
+++ line_scanner.c
@@ -70,7 +70,7 @@ void safequit(stack_t *arrayStack, int code, int line, char *command)
 	if (code == 3)
 		dprintf(STDERR_FILENO, "L%i: can't pint, stack empty\n", line);
 	if (code == 4)
-		dprinft(STDERR_FILENO, "L%i: can't pop an empty stack\n", line);
+		dprintf(STDERR_FILENO, "L%i: can't pop an empty stack\n", line);
 	free(command);
 	exit(EXIT_FAILURE);
 
